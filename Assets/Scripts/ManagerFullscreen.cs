bfc7e50df4ac88c6de3d6db2654d874b22f01be5
Stephen Pearce <hello@stevepdp.dev>
2022-04-02 10:16:26 +0100

    add window and fullscreen switching management

---
+++ Assets/Scripts/ManagerFullscreen.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerFullscreen : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftAlt) && Input.GetKeyDown(KeyCode.Return))
+        {
+            if (Screen.fullScreen)
+            {
+                Screen.SetResolution(640, 460, false);
+            }
+            else
+            {
+                Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, true);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/StartWindowed.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartWindowed : MonoBehaviour
+{
+    private void Awake()
+    {
+        Screen.fullScreen = false;
+    }
+}
