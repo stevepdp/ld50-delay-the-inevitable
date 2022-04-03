05cdb276b7d09a75622995ffaa52672ff8536f03
Stephen Pearce <hello@stevepdp.dev>
2022-04-04 00:08:23 +0100

    add projectiles, fire velocity and movement towards Earth

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,12 +7,20 @@ public class PlayerController : MonoBehaviour
     private float horizontalInput;
     private float verticalInput;
 
-    private float rotationSpeed = 90f;
+    private float projectileSpeed = .5f;
+    private float rotationSpeed = 90f;  
     private float verticalMoveSpeed = 2f;
-    
+
+    public GameObject bulletObj;
     public GameObject earthObj;
 
     private void Update()
+    {
+        Movement();
+        Fire();
+    }
+
+    private void Movement()
     {
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
@@ -22,5 +30,16 @@ public class PlayerController : MonoBehaviour
 
         // handle vertical movement
         transform.Translate(Vector2.up * verticalInput * verticalMoveSpeed * Time.deltaTime);
+    }
+
+    private void Fire()
+    {
+        if (Input.GetButtonDown("Fire1") || Input.GetKeyDown(KeyCode.Space))
+        {
+            // Instantiante and store it as temporal Rigidbody2D
+            Rigidbody2D projectileRbody = Instantiate(bulletObj, transform.position, transform.rotation).GetComponent<Rigidbody2D>();
+            // AddForce to the stored Rigidbody2D
+            projectileRbody.AddForce(-transform.position * projectileSpeed, ForceMode2D.Impulse);
+        }
     }
 }
