a1fe87a88a7d08fd74b95f4c30c6bd40603293c6
Stephen Pearce <hello@stevepdp.dev>
2022-04-04 17:11:37 +0100

    bullets destroy shuttles
    This isn's the ideal implementation...but I'm
    running out of time.
    
    If time permits, projectiles will send them
    back to the planet opposed to destroying them

--- Assets/Scripts/ShuttleController.cs
+++ Assets/Scripts/ShuttleController.cs
@@ -11,4 +11,13 @@ public class ShuttleController : MonoBehaviour
         // Keep moving on Y axis (towards calling planet, angle is determined at spawn time)
         transform.Translate(Vector2.up * f_ShuttleSpeed * Time.deltaTime);
     }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Bullet"))
+        {
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
 }
